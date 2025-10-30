4edc52db9be1717dc98390c7fcd0e4b1f9095105
ИСП-31 <-31@LAB17-05>
2025-10-30 10:41:56 +0300

    ааааааааааааааааааааааааааааааа

--- rpm8/MainWindow.xaml.cs
+++ rpm8/MainWindow.xaml.cs
@@ -20,5 +20,45 @@ namespace rpm8
         {
             InitializeComponent();
         }
+
+        struct Staff
+        {
+            public int Code;
+            public string Name;
+            public string Gender;
+            public string Post;
+            public double Exp;
+            public double Salary;
+
+            public Staff(int code, string name, string gender, string post, double exp, double salary)
+            {
+                Code = code;
+                Name = name;
+                Gender = gender;
+                Post = post;
+                Exp = exp;
+                Salary = salary;
+            }
+            public override string ToString()
+            {
+                return $"ФИО:{Name,40}, Пол:{Gender,7}, Должность:{Post,45}, Стаж работы:{Exp,50:F1} лет, Оклад:{Salary,7:F2}.";
+            }
+        }
+
+        Staff[] staff;
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+            lbStaff.ItemsSource = null;
+            int pos = Convert.ToInt32(tbCode.Text);
+            //staff[pos].Code = Convert.ToInt32(tbCode.Text); не работает
+            staff[pos].Name = tbFullName.Text;
+            staff[pos].Gender = tbGender.Text;
+            staff[pos].Post = tbPost.Text;
+            staff[pos].Exp = Convert.ToDouble(tbExp.Text);
+            staff[pos].Salary = Convert.ToDouble(tbSalary.Text);
+            lbStaff.ItemsSource = staff;
+
+        }
     }
 }
