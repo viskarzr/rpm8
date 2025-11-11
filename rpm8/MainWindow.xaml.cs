361d5259347863d5abbb8b2bdbec73bc2cd75bc0
ИСП-31 <-31@LAB17-05>
2025-11-11 13:49:04 +0300

    RPM8 Структуры

--- rpm8/MainWindow.xaml.cs
+++ rpm8/MainWindow.xaml.cs
@@ -23,6 +23,9 @@ namespace rpm8
             ShowStaff();
         }
 
+        /// <summary>
+        /// Структура
+        /// </summary>
         struct Staff
         {
             public int Code;
@@ -49,6 +52,9 @@ namespace rpm8
 
         Staff[] staff1;
 
+        /// <summary>
+        /// Создание структуры 
+        /// </summary>
         private void InitializeStaff()
         {
             staff1 = new Staff[7]
@@ -62,46 +68,70 @@ namespace rpm8
                 new Staff(6, "", "", "", 0, 0)
             };
         }
+
+        /// <summary>
+        /// Вывод структуры в листбокс
+        /// </summary>
         private void ShowStaff()
         {
             lbStaff.ItemsSource = staff1;
         }
+
+        /// <summary>
+        /// Изменение значений в структуре
+        /// </summary>
         private void btnChange_Click(object sender, RoutedEventArgs e)
         {
+            bool f1, f2,f3;
             lbStaff.ItemsSource = null;
-            int pos = Convert.ToInt32(tbCode.Text);
-            staff1[pos].Code = Convert.ToInt32(tbCode.Text); 
-            staff1[pos].Name = tbFullName.Text;
-            staff1[pos].Gender = tbGender.Text;
-            staff1[pos].Post = tbPost.Text;
-            staff1[pos].Exp = Convert.ToDouble(tbExp.Text);
-            staff1[pos].Salary = Convert.ToDouble(tbSalary.Text);
+            f1 = Int32.TryParse(tbCode.Text, out int code);
+            f2 = Double.TryParse(tbExp.Text, out double exp);
+            f3 = Double.TryParse(tbSalary.Text, out double salary);
+            if (f1 == f2 == f3 == true && code>=0 && code< staff1.Length && exp>=0 && salary>=0)
+            { 
+                int pos = code;
+                staff1[pos].Code = code; 
+                staff1[pos].Name = tbFullName.Text;
+                staff1[pos].Gender = tbGender.Text;
+                staff1[pos].Post = tbPost.Text;
+                staff1[pos].Exp = exp;
+                staff1[pos].Salary = salary;
+            }
+            else
+            {
+                MessageBox.Show("Проверьте корректность введенных данных!!!");
+            }
             lbStaff.ItemsSource = staff1;
-
         }
 
+        /// <summary>
+        /// Кнопка выхода
+        /// </summary>
         private void btnExit_Click(object sender, RoutedEventArgs e)
         {
             this.Close();   
         }
 
+        /// <summary>
+        /// Кнопка о программе
+        /// </summary>
         private void btnAbout_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Практическая работа №8\r\nРабота с типом данных структура.\r\nЗаполнить таблицу со списком сотрудников на 7 человек с полями: ФИО, пол, \r\nдолжность, стаж работы, оклад. Вывести результат на экран.\r\nВывести средний оклад.\r\nВыполнила:\r\nСтудентка гр.ИСП-31\r\nКирюшова В.");
         }
 
+        /// <summary>
+        /// Удаление значений в листбоксе
+        /// </summary>
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-            if (int.TryParse(tbCode.Text, out int pos) && pos >= 0 && pos < staff1.Length)
+            bool f1 = int.TryParse(tbCode.Text, out int pos);
+            if (f1 == true && pos>=0 && pos<7)
             {
-                // Reset the staff member at the specified position
-                staff1[pos] = new Staff(pos, "", "", "", 0, 0);
 
-                // Refresh the display
+                staff1[pos] = new Staff(pos, "", "", "", 0, 0);
                 lbStaff.ItemsSource = null;
                 lbStaff.ItemsSource = staff1;
-
-                // Clear input fields
                 ClearInputFields();
             }
             else
@@ -110,24 +140,33 @@ namespace rpm8
             }
         }
 
+        /// <summary>
+        /// Метод очистки полей листбокса
+        /// </summary>
         private void ClearInputFields()
         {
-            tbCode.Text = "";
-            tbFullName.Text = "";
-            tbGender.Text = "";
-            tbPost.Text = "";
-            tbExp.Text = "";
-            tbSalary.Text = "";
+            tbCode.Clear();
+            tbFullName.Clear();
+            tbGender.Clear();
+            tbPost.Clear();
+            tbExp.Clear();
+            tbSalary.Clear();
         }
 
+        /// <summary>
+        /// Кнопка расчета среднего значения оклада
+        /// </summary>
         private void btnCalc_Click(object sender, RoutedEventArgs e)
         {
             double sal = 0;
             int count = 0;
             for (int i = 0; i<7; i++)
             {
-                sal += staff1[i].Salary;      
-                count++;
+                sal += staff1[i].Salary;
+                if (staff1[i].Salary>0)
+                {  
+                    count++; 
+                }
             }
             sal = sal / count;
             tbSal.Text = sal.ToString();
